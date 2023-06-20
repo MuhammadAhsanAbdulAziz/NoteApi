b7bf9fa8a45fc8f2cfa5a1d57708c358527e999b
muhammadahsanabdulaziz <maahsan36@gmail.com>
2023-06-20 23:49:34 +0500

    make it better

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
-
+const bcrypt = require("bcryptjs")
 
 
 app.use(express.json());
